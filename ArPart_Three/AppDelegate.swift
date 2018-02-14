010a60510098ecf287820ef51bcb0ee3e91e7ec7
Vishal Lohia <vishal.lohia@mobileprogrammingllc.com>
2018-02-14 20:14:36 +0530

    Code cleanup commit test

--- ArPart_Three/AppDelegate.swift
+++ ArPart_Three/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder {
     }
 }
 
+//Delegates
 extension AppDelegate: UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
